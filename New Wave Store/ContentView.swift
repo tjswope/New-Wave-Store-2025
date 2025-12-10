3a5d9aac29ce4f82e176e469c17a59ff4c4ac993
Tom Swope <tjswope@gmail.com>
2025-12-10 10:38:16 -0500

    Initial Commit

---
+++ New Wave Store/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  New Wave Store
+//
+//  Created by Swope, Thomas on 12/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ New Wave Store/New_Wave_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_StoreApp.swift
+//  New Wave Store
+//
+//  Created by Swope, Thomas on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
